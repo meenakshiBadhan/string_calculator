e83bd7051eb4f51f1ff4086ba1a49f9f856912ca
Meenakshi Badhan <minileo1@gmail.com>
2025-07-13 14:40:53 +0530

    Added test case for input as a single number
    - Split input using commas and convert to integers before calculating the sum
    - Added test case to add single number
    
    Author: Meenakshi Badhan

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,5 +2,11 @@ class Calculator
   # Calculates the sum of numbers
   def add(numbers)
     return 0 if numbers.empty?
+
+    # Split using commas and convert to integers
+    nums = numbers.split(',').map(&:strip).map(&:to_i)
+
+    # Return sum
+    nums.sum
   end
 end
--- spec/services/calculator_test.rb
+++ spec/services/calculator_test.rb
@@ -7,5 +7,9 @@ RSpec.describe Calculator, type: :service do
     it 'returns 0 for empty string' do
       expect(calculator.add("")).to eq(0)
     end
+    
+    it 'returns the number itself for a single number' do
+      expect(calculator.add("1")).to eq(1)
+    end
   end
 end
